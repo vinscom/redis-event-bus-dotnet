56ee1652b7104cf4fbe23541504ab588c6b04e48
Vinay <vinaymail@gmail.com>
2024-05-10 13:25:05 +0100

    India Commit

---
+++ RedisEventBus/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace RedisEventBus
+{
+    public class Class1
+    {
+        public void SampleMethod()
+        {
+            // TODO: Implement the sample method
+        }
+    }
+}
---
+++ RedisEventBusTests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedisEventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisEventBus.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void SampleMethodTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
